8ad2447e27ca1fac51f55fa718c66812f871c079
Joohyung Park <johy@dabyeol.com>
2022-10-28 18:50:20 +0900

    Implement item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,34 @@
+import { Card } from "react-bootstrap";
+import { useEffect } from "react";
+import { run } from "holderjs";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Item() {
+function Item({ name, price }) {
+  useEffect(() => {
+    run();
+  });
+
   return (
-    <>
-      아이템(메뉴) 컴포넌트 <Link to="/options">메뉴명</Link>
-    </>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{price.toLocaleString()}원</Card.Text>
+      </Card.Body>
+      <Link to="/options" className="stretched-link" />
+    </Card>
   );
 }
+
+Item.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.number,
+};
+
+Item.defaultProps = {
+  name: "요거트",
+  price: 5000,
+};
 
 export default Item;
