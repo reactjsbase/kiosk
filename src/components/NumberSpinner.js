415fc80c1c7defb09d3189ef3bc098a853b37c53
Joohyung Park <johy@dabyeol.com>
2022-10-28 18:50:40 +0900

    Add NumberSpinner

---
+++ src/components/NumberSpinner.js
@@ -0,0 +1,40 @@
+import { Button, Form, InputGroup } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { BsDash, BsPlus } from "react-icons/bs";
+
+function NumberSpinner({ value, min, max, onChange }) {
+  function onIncrease(e) {
+    if (value < max) onChange(++value);
+  }
+
+  function onDecrease(e) {
+    if (value > min) onChange(--value);
+  }
+
+  return (
+    <InputGroup>
+      <Button variant="outline-secondary" onClick={onDecrease}>
+        <BsDash />
+      </Button>
+      <Form.Control className="text-center" value={value} readOnly />
+      <Button variant="outline-secondary" onClick={onIncrease}>
+        <BsPlus />
+      </Button>
+    </InputGroup>
+  );
+}
+
+NumberSpinner.propTypes = {
+  value: PropTypes.number,
+  min: PropTypes.number,
+  max: PropTypes.number,
+  onChange: PropTypes.func,
+};
+
+NumberSpinner.defaultProps = {
+  value: 1,
+  min: 1,
+  max: 100,
+};
+
+export default NumberSpinner;
