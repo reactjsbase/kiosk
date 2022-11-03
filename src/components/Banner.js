01404c3e6c1ed3894b9ccc5e456817c7e3d0cf48
Joohyung Park <johy@dabyeol.com>
2022-11-03 20:30:46 +0900

    Implement banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,13 @@
+import { run } from "holderjs";
+import { useEffect } from "react";
+import { Image } from "react-bootstrap";
+
 function Banner() {
-  return <h1>배너</h1>;
+  useEffect(() => {
+    run();
+  });
+
+  return <Image height={180} variant="top" src="holder.js/100px180" />;
 }
 
 export default Banner;
