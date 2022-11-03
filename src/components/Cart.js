64ca77ad165b0dfee159ee99be3aaefc8a71b61d
Joohyung Park <johy@dabyeol.com>
2022-11-03 20:45:25 +0900

    Improve Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,20 @@
+import { Col, Container, Row } from "react-bootstrap";
 import CartItem from "./CartItem";
 
 // 장바구니 컨테이너
 function Cart() {
   return (
-    <div className="d-flex flex-row">
-      <CartItem />
-      <CartItem />
-      <CartItem />
-      <CartItem />
+    <div style={{ background: "AntiqueWhite" }}>
+      <Container className="p-3">
+        <h3>카트</h3>
+        <Row xs={3} className="d-flex flex-row flex-nowrap overflow-auto">
+          {Array.from({ length: 6 }).map(() => (
+            <Col>
+              <CartItem />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
