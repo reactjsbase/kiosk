b70c3023035031632b75983caf2059be3502b2c3
Hyeseo20 <hskim0415@naver.com>
2022-11-07 01:20:18 +0900

    implement option

--- src/components/ConfirmItem.js
+++ src/components/ConfirmItem.js
@@ -1,5 +1,24 @@
+import React, { useState } from 'react' 
+import Nav from './components/Nav';
+import './App.css'
+import {
+BrowserRouter as Router, Switch, Route,
+} from "react-router-dom";
 function ConfirmItem() {
-  return <>주문내역 컴포넌트</>;
+const [cartItems, setCartItems] = useState(initialState.cartItems);
+return (
+<Router>
+<Nav cartItems={cartItems}/>
+<Switch>
+<Route exact={true} path="/">
+<ItemListContainer items={items} setCartItems={setCartItems} cartItems={cartItems }/>
+{/* props로 배열 전달 */}
+</Route>
+<Route path=" /Cart">
+<ShoppingCart cartItems={cartItems} items={items} setCartItems={setCartItems}/>
+</Route>
+</Switch>
+</Router>
+);
 }
-
 export default ConfirmItem;
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,6 +1,47 @@
-// 체크박스 형식의 옵션
-function Option() {
-  return <>옵션컴포넌트</>;
+import { Card, Col, Image, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { run } from "holderjs";
+import PropTypes from "prop-types";
+import NumberSpinner from "./NumberSpinner";
+
+function Option({ name, price }) {
+  const [quantity, setQuantity] = useState(1);
+
+  useEffect(() => {
+    run();
+  });
+
+  function onChange(value) {
+    setQuantity(value);
+  }
+
+  return (
+    <Card>
+      <Row className="g-0">
+        <Col xs={5}>
+          <Image className="rounded-start" src="holder.js/100px180" />
+        </Col>
+        <Col>
+          <Card.Body>
+            
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>{(price * quantity).toLocaleString()}원</Card.Text>
+            <NumberSpinner value={quantity} onChange={onChange} />
+          </Card.Body>
+        </Col>
+      </Row>
+    </Card>
+  );
 }
+
+Option.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.number,
+};
+
+Option.defaultProps = {
+  name: "그래놀라",
+  price: 300,
+};
 
 export default Option;
