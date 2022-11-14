13fbadd27e8055df5448b8339bfdbe5e6b8a01a0
jisoo3 <jisu032405@naver.com>
2022-11-15 01:02:23 +0900

    feat: change styles of order buttons

--- src/routes/Order.js
+++ src/routes/Order.js
@@ -1,17 +1,37 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Button, Row, Container, Stack } from "react-bootstrap";
 import Banner from "../components/Banner";
 import Cart from "../components/Cart";
 import Menu from "../components/Menu";
 
 function Order({ cart, addCartItem, removeCartItem }) {
+  const navigate = useNavigate();
+
   return (
     <>
       <Banner />
       <Menu addCartItem={addCartItem} />
       <Cart cart={cart} removeCartItem={removeCartItem} />
-      <h1>
-        <Link to="/">취소</Link> <Link to="/confirm">결제</Link>
-      </h1>
+      <Container fluid className="fixed-bottom bottom-0 bg-white">
+        <Stack gap={3} className="p-3">
+          <Row>
+            <Button variant="outline-danger" size="lg">
+              취소
+            </Button>
+          </Row>
+          <Row>
+            <Button
+              variant="primary"
+              size="lg"
+              onClick={() => {
+                navigate("/payment");
+              }}
+            >
+              결제
+            </Button>
+          </Row>
+        </Stack>
+      </Container>
     </>
   );
 }
