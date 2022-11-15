4754b7b932f320bb82b22210a34561543dd08c82
Joohyung Park <johy@dabyeol.com>
2022-11-16 00:25:33 +0900

    Minor changes

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { run } from "holderjs";
 import PropTypes from "prop-types";
 import NumberSpinner from "./NumberSpinner";
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
 
 function Option({ name, price }) {
   const [quantity, setQuantity] = useState(1);
@@ -17,49 +17,44 @@ function Option({ name, price }) {
   }
 
   return (
-      <Dropdown.Menu show>
-        <Dropdown.Header>
-          잼+꿀
-        </Dropdown.Header>
-        <Dropdown.Item eventKey="2">
-          <Card>
-           <Row className="g-0">
+    <Dropdown.Menu show>
+      <Dropdown.Header>잼+꿀</Dropdown.Header>
+      <Dropdown.Item eventKey="2">
+        <Card>
+          <Row className="g-0">
             <Col xs={5}>
               <Image className="rounded-start" src="holder.js/100px180" />
             </Col>
             <Col>
               <Card.Body>
-               <Card.Title>{name}</Card.Title>
-               <Card.Text>{(price * quantity).toLocaleString()}원</Card.Text>
-               <NumberSpinner value={quantity} onChange={onChange} />
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>{(price * quantity).toLocaleString()}원</Card.Text>
+                <NumberSpinner value={quantity} onChange={onChange} />
               </Card.Body>
             </Col>
           </Row>
-           </Card> 
-        </Dropdown.Item>
-        <Dropdown.Item eventKey="3"> 
+        </Card>
+      </Dropdown.Item>
+      <Dropdown.Item eventKey="3">
         그래놀라
         <Card>
-           <Row className="g-0">
+          <Row className="g-0">
             <Col xs={5}>
               <Image className="rounded-start" src="holder.js/100px180" />
             </Col>
             <Col>
               <Card.Body>
-               <Card.Title>{name}</Card.Title>
-               <Card.Text>{(price * quantity).toLocaleString()}원</Card.Text>
-               <NumberSpinner value={quantity} onChange={onChange} />
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>{(price * quantity).toLocaleString()}원</Card.Text>
+                <NumberSpinner value={quantity} onChange={onChange} />
               </Card.Body>
             </Col>
           </Row>
-           </Card> 
-        
-        </Dropdown.Item>
-     </Dropdown.Menu>
-   );
-  }
-
-
+        </Card>
+      </Dropdown.Item>
+    </Dropdown.Menu>
+  );
+}
 
 Option.propTypes = {
   name: PropTypes.string,
