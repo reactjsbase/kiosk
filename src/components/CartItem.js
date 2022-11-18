cd4b4723b87f5a4f5df4681dcb2dd618ecd9ca47
Joohyung Park <johy@dabyeol.com>
2022-11-19 03:54:06 +0900

    Improve cart item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,15 +1,10 @@
 import { Card, CloseButton, Col, Image, Row } from "react-bootstrap";
-import { useEffect, useState } from "react";
-import { run } from "holderjs";
+import { useState } from "react";
 import NumberSpinner from "./NumberSpinner";
-// import menuData from "../data/menuData.json";
+import bowl from "../images/bowl.png";
 
 function CartItem({ index, data, onRemove }) {
   const [quantity, setQuantity] = useState(1);
-
-  useEffect(() => {
-    run();
-  });
 
   function handleChange(value) {
     setQuantity(value);
@@ -17,23 +12,35 @@ function CartItem({ index, data, onRemove }) {
 
   return (
     <Card>
+      <Card.Header>
+        {data.title}
+        <CloseButton className="float-end" onClick={() => onRemove(index)} />
+      </Card.Header>
       <Row className="g-0">
         <Col xs={5}>
-          <Image className="rounded-start" src="holder.js/100px180" />
+          <Image
+            width="100%"
+            height={128}
+            src={bowl}
+            style={{
+              objectFit: "contain",
+              background: "WhiteSmoke",
+            }}
+          />
         </Col>
         <Col>
-          <Card.Body>
-            <Card.Text>
-              <CloseButton
-                className="float-end"
-                onClick={() => onRemove(index)}
-              />
-            </Card.Text>
-            <Card.Title>{data.title}</Card.Title>
-            <Card.Text>{(data.price * quantity).toLocaleString()}원</Card.Text>
-            <NumberSpinner value={quantity} onChange={handleChange} />
+          <Card.Body style={{ height: "100%" }} className="d-flex flex-column">
+            <Card.Subtitle className="mb-2 text-muted">옵션목록</Card.Subtitle>
+            <NumberSpinner
+              className="mt-auto"
+              value={quantity}
+              onChange={handleChange}
+            />
           </Card.Body>
         </Col>
+        <Card.Footer className="text-end">
+          {(data.price * quantity).toLocaleString()}원
+        </Card.Footer>
       </Row>
     </Card>
   );
