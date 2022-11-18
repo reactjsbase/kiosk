6e23001ce01b7f668d78ae2edfe8c31bbef7c705
Joohyung Park <johy@dabyeol.com>
2022-11-18 19:29:48 +0900

    Implement item image

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,7 @@
 import { Card } from "react-bootstrap";
 import { useEffect } from "react";
 import { run } from "holderjs";
+import bowl from "../images/bowl.png";
 
 function Item({ data, onClick }) {
   useEffect(() => {
@@ -9,9 +10,27 @@ function Item({ data, onClick }) {
 
   return (
     <Card onClick={() => onClick(data)}>
-      <Card.Img height={180} variant="top" src="holder.js/100px180" />
+      <div style={{ position: "relative" }}>
+        <Card.Img
+          src={bowl}
+          height={180}
+          style={{
+            objectFit: "contain",
+            background: "WhiteSmoke",
+          }}
+          variant="top"
+        />
+        <div style={{ position: "absolute", top: 0 }}>
+          <Card.Img
+            height={180}
+            style={{ objectFit: "contain" }}
+            src={require(`../images/${data.id}.png`)}
+          />
+        </div>
+      </div>
       <Card.Body>
         <Card.Title>{data.title}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{data.gram}g</Card.Subtitle>
         <Card.Text>{data.price.toLocaleString()}원</Card.Text>
       </Card.Body>
     </Card>
