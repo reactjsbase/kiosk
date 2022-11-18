9a94c76d7d807a606a301050654ea90785a9df5c
Joohyung Park <johy@dabyeol.com>
2022-11-18 19:28:28 +0900

    Update banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,18 @@
 import { run } from "holderjs";
 import { useEffect } from "react";
 import { Image } from "react-bootstrap";
+import banner from "../images/banner.png";
 
 function Banner() {
   useEffect(() => {
     run();
   });
 
-  return <Image height={180} variant="top" src="holder.js/100px180" />;
+  return (
+    <div style={{ textAlign: "center", background: "SeaGreen" }}>
+      <Image height="180" variant="top" src={banner} />
+    </div>
+  );
 }
 
 export default Banner;
