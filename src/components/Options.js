50ae2b6ca1e1e82ab237573006790b2c2514d3ed
Joohyung Park <johy@dabyeol.com>
2022-11-21 20:02:09 +0900

    Add bowl image in options

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,6 +1,7 @@
-import { Button, Modal } from "react-bootstrap";
+import { Button, Image, Modal } from "react-bootstrap";
 import CartItem from "./CartItem";
 import Option from "./Option";
+import bowl from "../images/bowl.png";
 
 // 옵션 리스트 보여주는 화면
 function Options({ itemData, onAdd, ...props }) {
@@ -10,6 +11,16 @@ function Options({ itemData, onAdd, ...props }) {
         <Modal.Title>옵션 선택</Modal.Title>
       </Modal.Header>
       <Modal.Body>
+        <Image
+          src={bowl}
+          width="100%"
+          height={160}
+          style={{
+            objectFit: "contain",
+            background: "WhiteSmoke",
+          }}
+        />
+
         <CartItem data={itemData} />
         <h2>옵션선택</h2>
         {/* 옵션 컨테이너 */}
