a9fb38134bcdb5a4d7dfe599f46ef911556aab23
Joohyung Park <johy@dabyeol.com>
2022-11-23 04:02:04 +0900

    Minor changes

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -4,7 +4,7 @@ import { Col, Row } from "react-bootstrap";
 // 아이템 컨테이너
 function MenuList({ items, onItemClick }) {
   return (
-    <Row xs={2} lg={3} xxl={4} className="g-4">
+    <Row xs={2} md={3} lg={4} className="g-3">
       {items.map((itemData, index) => (
         <Col key={index}>
           <Item data={itemData} onClick={onItemClick} />
--- src/routes/Payment.js
+++ src/routes/Payment.js
@@ -5,16 +5,14 @@ import Banner from "../components/Banner";
 function Payment() {
   const navigate = useNavigate();
   return (
-    <>
-      <div className="vh-100 d-flex flex-column">
-        <Banner />
-        <h2>결제</h2>
-        결제완료
-        <Button variant="dark" size="lg" onClick={() => navigate("/")}>
-          처음 화면으로
-        </Button>
-      </div>
-    </>
+    <div className="vh-100 d-flex flex-column">
+      <Banner />
+      <h2>결제</h2>
+      결제완료
+      <Button variant="dark" size="lg" onClick={() => navigate("/")}>
+        처음 화면으로
+      </Button>
+    </div>
   );
 }
 
