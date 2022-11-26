0b9f5cd66455fb58adcb8f4156837b712aaeadda
jisoo3 <jisu032405@naver.com>
2022-11-27 01:55:48 +0900

    modify home and payment

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,24 +8,23 @@ function Home() {
   return (
     <div className="vh-100 d-flex flex-column">
       <Banner />
-      <Stack gap={3} className="p-3 h-100">
-        <Button
-          variant="outline-dark"
-          size="lg"
-          onClick={() => navigate("/order")}
-          className="h-100"
-        >
-          매장
-        </Button>
-
-        <Button
-          variant="outline-dark"
-          size="lg"
-          onClick={() => navigate("/order")}
-          className="h-100"
-        >
-          포장
-        </Button>
+      <Stack gap={3} className="p-3 h-500">
+        <div className="product-container">
+          <div className="row">
+            <div style={{display:"flex" ,margin:"100px 0px", justifyContent:'center' ,cursor:"pointer"}} onClick={() => navigate("/order")}>
+              <h1 style={{fontSize:"50px"}}>매장</h1>
+              <img src="https://cdn-icons-png.flaticon.com/512/831/831417.png" style={{width:"50px", height:"50px"}}/>
+            </div>
+            <div style={{display:"flex" ,padding:"15px 0px", backgroundColor: "#f5f5dc", alignItems:"center",justifyContent:'center'}}>
+              <img src="https://cdn-icons-png.flaticon.com/512/1944/1944379.png" style={{width:"50px", height:"50px"}}/>
+              <h2 style={{fontSize:"32px"}}>주문 방식을 선택해주세요</h2>
+            </div>
+            <div style={{display:"flex" ,margin:"150px 0px 0px 0px", justifyContent:'center',cursor:"pointer"}} onClick={() => navigate("/order")}>
+              <h1 style={{fontSize:"50px"}}>포장</h1>
+              <img src="https://cdn-icons-png.flaticon.com/512/2558/2558149.png" style={{width:"50px", height:"50px"}}/>
+            </div>
+          </div>
+        </div>
       </Stack>
     </div>
   );
--- src/routes/Order.js
+++ src/routes/Order.js
@@ -7,6 +7,20 @@ import Menu from "../components/Menu";
 function Order({ cart, addCartItem, removeCartItem }) {
   const navigate = useNavigate();
 
+  const priceAdd =() => {
+    const itemPrice = cart.map((item)=>{
+      return item.price
+    })
+
+    const result = itemPrice.reduce(function add(sum, currValue) {
+      return sum + currValue;
+    }, 0);
+
+    return result
+  }
+
+
+
   return (
     <div className="vh-100 d-flex flex-column">
       <Banner />
@@ -17,20 +31,40 @@ function Order({ cart, addCartItem, removeCartItem }) {
       <div>
         <Cart cart={cart} removeCartItem={removeCartItem} />
         <Stack gap={3} className="p-3">
-          <Button
-            variant="outline-danger"
-            size="lg"
-            onClick={() => navigate("/")}
-          >
-            취소
-          </Button>
-          <Button
-            variant="primary"
-            size="lg"
-            onClick={() => navigate("/confirm")}
-          >
-            결제
-          </Button>
+          <div className="row justify-content-start">
+            <div className="card col-8">
+              <div className="card-body">
+                <span style={{paddingRight:"10px"}}>수량</span>
+                <span>{cart.length.toLocaleString()}</span>
+              </div>
+            </div>
+            <div class="d-grid col-4 mx-auto">
+              <Button
+                variant="outline-danger"
+                size="lg"
+                onClick={() => navigate("/")}
+              >
+                취소
+              </Button>
+            </div>
+          </div>
+          <div className="row justify-content-start">
+            <div className="card col-8">
+              <div className="card-body">
+                <span style={{paddingRight:"10px"}}>합계</span>
+                <span>{priceAdd().toLocaleString()}</span>
+              </div>
+            </div>
+            <div className="d-grid col-4 mx-auto">
+              <Button
+                variant="primary"
+                size="lg"
+                onClick={() => navigate("/confirm")}
+              >
+                결제
+              </Button>
+            </div>
+          </div>
         </Stack>
       </div>
     </div>
